fb1db72da214d5e6f00605dec187fb2b3876985c
kapil234 <109575291+kapil234@users.noreply.github.com>
2026-06-27 13:25:43 +0530

    Header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="bg-blue-600 text-white py-4 rounded-t-xl">
+      <h1 className="text-3xl font-bold text-center">
+        To-Do List Application
+      </h1>
+    </header>
+  );
+}
+
+
+
+export default Header
